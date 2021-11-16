806aa7f01df5e50c54478ebe33756dd65ce8233c
rohankumardubey <rohankumardubey497@gmail.com>
2021-11-16 15:06:25 +0530

    updates

--- src/main/java/com/coronavirus/coronavirustracker/CoronavirusTrackerApplication.java
+++ src/main/java/com/coronavirus/coronavirustracker/CoronavirusTrackerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+
 @SpringBootApplication
 //enable scheduling will use the cron expression used in the class for scheduling
 @EnableScheduling
@@ -12,5 +16,4 @@ public class CoronavirusTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoronavirusTrackerApplication.class, args);
 	}
-
 }
